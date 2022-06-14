7d825815c236eb475e6c237e1475186da22f023e
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2022-06-14 15:33:41 +0200

    Update brutal-R.py

--- brutal-R.py
+++ brutal-R.py
@@ -2,7 +2,13 @@ import os
 os.system("apt install figlet") 
 os.system("clear")
 os.system("figlet brutal-R")
-os.system("timeout 7 ping russia.com")
-os.system("timeout 3 ping www.news.ru")
-os.system("timeout 2 ping www.tvrain.ru") 
-
+print("1 install figlet")
+print("2 Start pinging Russia websites")
+x=input("brutal-R>")
+if x == 1:
+   os.system("apt install figlet")
+if x == 2:
+   os.system("timeout 7 ping russia.com")
+   os.system("timeout 3 ping www.news.ru")
+   os.system("timeout 2 ping www.tvrain.ru") 
+   x=input("brutal-R>")
