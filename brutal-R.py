6dbca2020b47c8f6d568aeae26a22943aa7b62ca
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2022-06-14 15:08:20 +0200

    Update and rename brutal-R.sh to brutal-R.py

---
+++ brutal-R.py
@@ -0,0 +1,8 @@
+import os
+os.system("apt install figlet") 
+os.system("clear")
+os.system("figlet brutal-R")
+os.system("timeout 7 ping russia.com")
+os.system("timeout 3 ping www.news.ru")
+os.system("timeout 2 ping www.tvrain.ru") 
+
