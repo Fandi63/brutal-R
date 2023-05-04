3ec75fb9e7c8b65e62f59a6d58b6b57d302c86b3
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2023-05-04 15:27:06 +0200

    Update brutal-R.py

--- brutal-R.py
+++ brutal-R.py
@@ -6,11 +6,6 @@ input("Press enter to ping Russia")
 os.system("timeout 7 ping russia.com") 
 os.system("timeout 3 ping www.news.ru")
 os.system("timeout 2 ping www.tvrain.ru")
-input("brutal-R.py=>")
-input("brutal-R.py=>")
-input("brutal-R.py=>")
-input("brutal-R.py=>")
-input("brutal-R.py=>")
-input("brutal-R.py=>")
+input("Press enter to exit.")
  
     
