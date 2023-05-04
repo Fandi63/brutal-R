0757dd223942c8fdf2113593dc9835ab41bc5e4a
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2023-05-04 15:46:02 +0200

    Update brutal-R.py

--- brutal-R.py
+++ brutal-R.py
@@ -2,6 +2,8 @@ import os
 os.system("apt install figlet") 
 os.system("clear")
 os.system("figlet brutal-R")
+print("A russia IP hacking system.")
+print("===========================")
 input("Press enter to ping Russia")
 os.system("timeout 7 ping russia.com") 
 os.system("timeout 3 ping www.news.ru")
